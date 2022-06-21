12eab9d9ad8ec832203f06c1617d101c8b8bef5e
Tinashe Muchie <tinashemuchineripi@gmail.com>
2022-06-21 18:46:17 +0400

    pages index

---
+++ src/Pages/index.ts
@@ -0,0 +1,7 @@
+export { HOME } from "./Home";
+export { PRODUCTS } from "./Products";
+export { ABOUT_US } from "./About_us";
+export { INTERIOR_DECOR } from "./Interior_Decor_Services";
+export { CONTACT_US } from "./Contact";
+export { SIGN_IN } from "./SignIn";
+export { CART } from "./Cart";
