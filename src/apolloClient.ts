94375d8a67606eda504e8835867e2672f20903dd
Tinashe Muchie <tinashemuchineripi@gmail.com>
2022-06-24 15:59:34 +0400

    apolloClient initilization

---
+++ src/apolloClient.ts
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri:'https://api-us-east-1.graphcms.com/v2/cl4qv02r4077301z383zi5brg/master',
+    cache: new InMemoryCache()
+})
+
+export default client;
