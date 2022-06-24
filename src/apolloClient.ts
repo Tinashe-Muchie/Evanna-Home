d80e43c6be707e857df74833c8ceb6ca6bb82159
Tinashe Muchie <tinashemuchineripi@gmail.com>
2022-06-24 16:20:03 +0400

    process.env api key

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri:'https://api-us-east-1.graphcms.com/v2/cl4qv02r4077301z383zi5brg/master',
+    uri:`https://api-us-east-1.graphcms.com/v2/${process.env.REACT_APP_API_KEY}/master`,
     cache: new InMemoryCache()
 })
 
