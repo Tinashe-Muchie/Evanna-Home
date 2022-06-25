bb42747c3734e28713c7fb5a448fc09013d97cc6
Tinashe Muchie <tinashemuchineripi@gmail.com>
2022-06-25 13:03:14 +0400

    contact pages formik addition

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -25,7 +25,7 @@ export const CONTACT_US = () => {
   return (
     <section className="contact_us_container">
       <h1>Contact Us</h1>
-      <form action="" className="form_wrapper">
+      <form onSubmit={formik.handleSubmit} className="form_wrapper">
         <header className="form_header_wrapper">
           <div>
             <span>Ask A Question</span>
