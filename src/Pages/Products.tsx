f95b23e257a0f71901bb7ebf3ba9186b6c0e0be4
Tinashe Muchie <tinashemuchineripi@gmail.com>
2022-06-25 13:02:09 +0400

    products page with products loaded

--- src/Pages/Products.tsx
+++ src/Pages/Products.tsx
@@ -1,3 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { gql} from "@apollo/client";
+import client from "../apolloClient";
+
+
 export const PRODUCTS = () => {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await client.query({
+        query: gql`
+        query MyQuery {
+          categories(first: 25) {
+            slug
+            title
+            id
+            product(first: 25) {
+              ... on Product {
+                id
+                name
+                availability
+                price
+                productDescription
+                slug
+                productPhoto {
+                  id
+                  url
+                }
+              }
+            }
+          }
+        }
+        `
+      });
+
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+  console.log(products)
+
   return <div>products page</div>;
 };
