000dc110c3736bb7d2e5194346e3c6140c6e38ae
Tinashe Muchie <tinashemuchineripi@gmail.com>
2022-06-27 14:00:16 +0400

    update onclick item

--- src/Pages/Products.tsx
+++ src/Pages/Products.tsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { gql} from "@apollo/client";
 import client from "../apolloClient";
+import { useLocation } from 'react-router-dom';
+import { MyQueryQuery } from '../generated/categories_generated';
+import { PRODUCT_CARD } from '../Components/ProductCard';
+import { Link } from 'react-router-dom';
 
 
 export const PRODUCTS = () => {
-  const [products, setProducts] = useState(null);
+  const [products, setProducts] = useState<MyQueryQuery | undefined>(undefined);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -39,7 +43,34 @@ export const PRODUCTS = () => {
 
     fetchProducts();
   }, []);
-  console.log(products)
 
-  return <div>products page</div>;
+  const location = useLocation();
+  const data = location.state;
+
+  const PRODUCTS = products?.categories.filter((category)=>category.slug === data)
+  console.log(PRODUCTS)
+
+  const product = PRODUCTS?.[0]
+  const items = product?.product
+  
+  return (
+    <div className="products_page_container">
+      <div className="product_title">{product?.title}</div>
+      <div className="products_wrapper">
+        {
+          items?.map((item)=> (
+            <div key={item.id}>
+              <Link 
+                to="/details"
+                state={item}
+                className="product_link"
+              >
+                <PRODUCT_CARD name={item.name} price={item.price} image={item.productPhoto} />
+              </Link>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
 };
