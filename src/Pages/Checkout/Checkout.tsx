d4874e34937df2883734699436e5b525a2078a27
Tinashe Muchie <tinashemuchineripi@gmail.com>
2022-07-01 19:27:38 +0400

    checkout page

---
+++ src/Pages/Checkout/Checkout.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { FaHome, FaCreditCard } from 'react-icons/fa';
+import { ADDRESSFORM } from './AddressForm';
+import { PAYMENT } from './Payment';
+import { CONFIRMATION } from './Confirmation';
+
+export const CHECKOUT = () => {
+
+    const [ active, setActive ] = useState(0);
+    const Next = () => {setActive((prevActiveStep) => prevActiveStep + 1)}
+    const Prev = () => {setActive((prevActiveStep) => prevActiveStep - 1)}
+
+    /*switch statement to alternate between address form, payment form and 
+  confirmation form*/
+  const getStep = (step: number) => {
+    switch(step) {
+      case 0:
+        return <ADDRESSFORM next={Next}/>
+      case 1:
+        return <PAYMENT 
+                  Next={Next}
+                  Prev={Prev}
+                  timeout={timeout}
+                />
+      case 2: 
+        return <CONFIRMATION 
+                  Done={done}
+                />
+      default: throw new Error()
+    }
+  }
+
+  const [done, setDone] = useState(false)
+  const timeout = () => {
+    setTimeout(()=>{
+      setDone(true)
+    }, 7000)
+  }
+
+    return (
+        <div className='stepper_wrapper'>
+            <div className='stepper_container'>
+                <div 
+                    className={(active===0) ? 'active_tab' : 'tab'}
+                > 
+                    <FaHome /> 
+                </div>
+                <div 
+                    className={(active===1) ? 'active_tab' : 'tab'}
+                >  
+                    <FaCreditCard /> 
+                </div> 
+            </div>
+            { getStep(active) }
+        </div>
+    );
+}
