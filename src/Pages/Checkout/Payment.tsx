ceb168b44bd46cbfff05548a4531ee24d93411d1
Tinashe Muchie <tinashemuchineripi@gmail.com>
2022-07-02 15:41:48 +0400

    payment page finalization

--- src/Pages/Checkout/Payment.tsx
+++ src/Pages/Checkout/Payment.tsx
@@ -3,29 +3,42 @@ import { Elements,
     CardElement, 
     useStripe, 
     useElements } from "@stripe/react-stripe-js";
+import { useCart } from "react-use-cart";
 
 const stripePromise = loadStripe('pk_test_51LGiq1I7VvZOo4gg8OPvK9PwWCY78TnMm4LjH9PMbDEhH5hjSLI9LmxxBEVcPVUhKBfLy7miu9qjRJ7xr2n2bHJr00QnhAInPb');
 
-const CheckoutForm = () => {
+const CheckoutForm = ({next, previous}: {next: ()=>void, previous: ()=> void}) => {
     const stripe = useStripe();
     const elements = useElements();
 
+    const { cartTotal } = useCart();
+
     return (
-        <form>
+        <form className="payment_wrapper">
             <CardElement />
-            <button
-                type="submit"
-                disabled={!stripe || !elements}
-            > Pay 
-            </button>
+            <div className="buttons_wrapper">
+                <button
+                    onClick={()=> previous()}
+                    className="back_button"
+                >
+                    Back
+                </button>
+                <button
+                    type="submit"
+                    disabled={!stripe || !elements}
+                    onClick={()=> next()}
+                    className="next_button"
+                > Pay R {cartTotal}
+                </button>
+            </div>    
         </form>
     );
 }
 
-export const PAYMENT = ({next, previous, timeout}: any) => {
+export const PAYMENT = ({next, prev}: any) => {
     return (
         <Elements stripe={stripePromise}>
-            <CheckoutForm />
+            <CheckoutForm next={next} previous={prev}/>
         </Elements>
     );
 };
