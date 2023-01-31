19f94f65bc087836fd00eb9048dabe8a47cd5b25
Tinashe Muchineripi <tinashemuchiineripi@gmail.com>
2023-01-31 13:20:02 +0200

    add refund polict page

---
+++ src/Pages/RefundPolicy.tsx
@@ -0,0 +1,30 @@
+export const REFUND_POLICY = () => {
+    return (
+        <div className="terms_of_service_container">
+            <h1>Refund Policy</h1>
+            <p>
+            We have a 7-day return policy, which means you have 7 days after receiving your item to request a return.
+            </p>
+            <p>
+            To be eligible for a return, your item must be in the same condition that you received it, unused, with tags, and in its original packaging. You’ll also need the receipt or proof of purchase.
+            </p>
+            <p>
+            To start a return, you can contact us at <strong>evannahomeware@yahoo.com</strong>. If your return is accepted, we’ll send you a return shipping label, as well as instructions on how and where to send your package. Items sent back to us without first requesting a return will not be accepted.
+            </p>
+            <p>
+            You can always contact us for any return question at <strong>evannahomeware@yahoo.com</strong>
+            </p>
+            <h2>Damages and issues</h2>
+            <p>
+            Please inspect your order upon reception and contact us immediately if the item is defective, damaged or if you receive the wrong item, so that we can evaluate the issue and make it right.
+            </p>
+            <p>
+            Unfortunately, we cannot accept returns on sale items or gift cards.
+            </p>
+            <h2>Refunds</h2>
+            <p>
+            We will notify you once we’ve received and inspected your return, and let you know if the refund was approved or not. If approved, you’ll be automatically refunded on your original payment method. Please remember it can take some time for your bank or credit card company to process and post the refund too.
+            </p>
+        </div>
+    );
+}
